41ab346ef1db02e5058fcc78eef6aadc4530c1ba
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-05-12 21:10:07 -0400

    test

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -7,32 +7,62 @@ namespace RPG.Core
     [System.Serializable]
     public class Condition
     {
-        [System.Serializable]
-        class Predicate
-        {
-
-        }
-        [SerializeField]string predicate;
-        [SerializeField]string[] praram;
+        [SerializeField]Disjunction[] and;
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            foreach(var evaluator in evaluators)
+            foreach(var dis in and)
             {
-                bool? result = evaluator.Evaluate(predicate, praram);
+                if(!dis.Check(evaluators)){return false; }
 
-                if(result == null)
-                {
-                    continue;
-                }
-                if(result == false)return false;
-
-               
-                
             }
-
             return true;
         }
+
+        [System.Serializable]
+        class Disjunction
+        {
+            [SerializeField]Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach(var pred in or)
+                {
+                    if(pred.Check(evaluators))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        [System.Serializable]
+        class Predicate
+        {
+            [SerializeField] string predicate;
+            [SerializeField] string[] praram;
+
+            [SerializeField]bool negate = false;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach(var evaluator in evaluators)
+                {
+                    bool? result = evaluator.Evaluate(predicate, praram);
+
+                    if(result == null)
+                    {
+                        continue;
+                    }
+                    
+                       if(result ==negate) return false;
+                }
+
+                return true;
+            }
+        }
+       
        
     }
 }
